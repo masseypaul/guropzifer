c16c88ba363ccc7f56bd40578a88523d3f952f8c
PenitenTangent <150669790+PenitenTangent@users.noreply.github.com>
2025-01-21 16:26:12 +0100

    data dir

--- solve_pb_continuous.py
+++ solve_pb_continuous.py
@@ -7,9 +7,9 @@ import pandas as pd
 import numpy as np
 import gurobipy as gp
 
-index_values = pd.read_csv("bricks_index_values.csv",index_col=0)
-distances = pd.read_csv("brick_rp_distances.csv",index_col=0)
-all_distances = pd.read_csv("distances.csv",delimiter=";")
+index_values = pd.read_csv("data/bricks_index_values.csv",index_col=0)
+distances = pd.read_csv("data/brick_rp_distances.csv",index_col=0)
+all_distances = pd.read_csv("data/distances.csv",delimiter=";")
 # pfitzer_100 = pd.read_csv("Pfitzer10-100.csv")
 
 #### change here the type of optimization
@@ -190,7 +190,7 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import MDS
 
 #ids is tha distance csv stripped of headers data
-distance_matrix= np.genfromtxt('dis.csv', delimiter=',', skip_header=0)  # skip_header=1 if there is a header
+distance_matrix= np.genfromtxt('data/dis.csv', delimiter=',', skip_header=0)  # skip_header=1 if there is a header
 
 
 keys = [4, 14, 16, 22]
