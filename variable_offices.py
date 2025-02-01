b67d41260e32c87a8fb3547e04d6c2a7cf6cfed5
Rubiksman78 <samuelabc78310@gmail.com>
2025-02-01 14:29:12 +0100

    fix_disrupt+total_sum

--- variable_offices.py
+++ variable_offices.py
@@ -15,7 +15,7 @@ distance_matrix= np.genfromtxt('data/dis.csv', delimiter=',', skip_header=0)  # 
 
 
 #### change here the type of optimization: Will be removed because 3-objectives
-OBJECTIVE = "dist" #or "disrupt" or "dist"
+OBJECTIVE = "disrupt" #or "disrupt" or "dist"
 
 representation = [
     [4,5,6,7,8,15],
@@ -24,9 +24,9 @@ representation = [
     [1,2,3,19,20,21,22]
 ]
 
-nb_SR = len(representation)
-nb_bricks = sum(len(repres) for repres in representation)
-new_work_balance = (len(representation))/nb_SR
+NB_SR = len(representation)
+N_BRICKS = sum(len(repres) for repres in representation)
+new_work_balance = (len(representation))/NB_SR
 
 representation_minus_one = [[x-1 for x in row] for row in representation]
 
@@ -35,116 +35,84 @@ workload = index_values
 
 # Create Model and Variables
 m = gp.Model("Step 1")
-v = m.addMVar(shape=(nb_bricks,nb_SR),vtype=gp.GRB.BINARY,name="V")
+v = m.addMVar(shape=(N_BRICKS,NB_SR),vtype=gp.GRB.BINARY,name="V")
 
-center_bricks = m.addVars(nb_bricks,vtype = gp.GRB.BINARY, name="C")
+center_bricks = m.addVars(N_BRICKS,vtype = gp.GRB.BINARY, name="C")
 
 # Create Constraints
 # Center brick constraints
 old_center = [3,13,15,21]
+old_center_one_hot = [1 if i in old_center else 0 for i in range(N_BRICKS)]
 
-m.addConstr(gp.quicksum(center_bricks[i] for i in range(len(center_bricks))) == nb_SR, name="nb center")
+m.addConstr(gp.quicksum(center_bricks[i] for i in range(len(center_bricks))) == NB_SR, name="nb center")
 
 # One SR per area
 m.addConstrs(
-    (sum(v[i, j] for j in range(nb_SR)) == 1 for i in range(nb_bricks)), 
+    (sum(v[i, j] for j in range(NB_SR)) == 1 for i in range(N_BRICKS)), 
     name="RowSum"
 )
 
 # Center can only belong to one SR
 m.addConstrs(
-    (gp.quicksum(center_bricks[i]*v[i,j] for i in range(nb_bricks)) == 1 for j in range(nb_SR)),
+    (gp.quicksum(center_bricks[i]*v[i,j] for i in range(N_BRICKS)) == 1 for j in range(NB_SR)),
     name="OCPSR" # One Center Per SR
 )
 
 
 # TO DO: Workload constraints: to reimplement as objective
-workload_repeated = np.repeat(workload,nb_SR,axis=0).reshape(nb_bricks,nb_SR)
-resulting_workload = m.addVars(nb_bricks,nb_SR,vtype=gp.GRB.CONTINUOUS, name="RW")
+workload_repeated = np.repeat(workload,NB_SR,axis=0).reshape(N_BRICKS,NB_SR)
+resulting_workload = m.addVars(N_BRICKS,NB_SR,vtype=gp.GRB.CONTINUOUS, name="RW")
 m.addConstrs(
-    (resulting_workload[i,j] == workload_repeated[i,j] * v[i,j] for i in range(nb_bricks) for j in range(nb_SR)),
+    (resulting_workload[i,j] == workload_repeated[i,j] * v[i,j] for i in range(N_BRICKS) for j in range(NB_SR)),
     name="workload"
 )
 
 minWL = 0.8*new_work_balance
 maxWL = 1.2*new_work_balance
 print(minWL,maxWL)
-weighted_sums = m.addVars(nb_SR, lb=0, vtype=gp.GRB.CONTINUOUS,name="WS")
+weighted_sums = m.addVars(NB_SR, lb=0, vtype=gp.GRB.CONTINUOUS,name="WS")
 m.addConstrs(
-    (weighted_sums[j] == gp.quicksum(resulting_workload[i,j] for i in range(nb_bricks)) for j in range(nb_SR)), 
+    (weighted_sums[j] == gp.quicksum(resulting_workload[i,j] for i in range(N_BRICKS)) for j in range(NB_SR)), 
     name="V=WS"
 )
 
 m.addConstrs(
-    (weighted_sums[j] >= minWL for j in range(nb_SR)), 
+    (weighted_sums[j] >= minWL for j in range(NB_SR)), 
     name="MinWeightedSums"
 )
 m.addConstrs(
-    (weighted_sums[j] <= maxWL for j in range(nb_SR)), 
+    (weighted_sums[j] <= maxWL for j in range(NB_SR)), 
     name="MaxWeightedSums"
 )
 
 
 #Create Objective
-#Objective minimum disruption
-m_orig = np.zeros((nb_bricks,nb_SR),dtype=int)
-for i in range(len(representation)):
-    m_orig[representation_minus_one[i],i] = 1
-    
-#Objective 1
 if OBJECTIVE == "disrupt":
-    m_orig = np.zeros((nb_bricks,nb_SR),dtype=int)
-    for i in range(nb_SR):
-        m_orig[old_center[i],i] = 1
-
-    diff_var = m.addVars(nb_bricks,nb_SR,vtype=gp.GRB.INTEGER,name="Diff")
-    m.addConstrs((diff_var[i,j] >= v[i,j] - m_orig[i,j] for i in range(nb_bricks) for j in range(nb_SR)),name="diff?")
-    abs_var = m.addVars(nb_bricks,nb_SR,vtype=gp.GRB.INTEGER,name="abs")
-    m.addConstrs((abs_var[i,j] == 0.5*gp.abs_(diff_var[i,j]) for i in range(nb_bricks) for j in range(nb_SR)),name="abs?")
-    disrupt_sum = gp.quicksum(abs_var[i,j] for i in range(nb_bricks) for j in range(nb_SR))
+    diff_var = m.addVars(N_BRICKS,vtype=gp.GRB.INTEGER,name="Diff")
+    m.addConstrs((diff_var[i] >= center_bricks[i] - old_center_one_hot[i] for i in range(N_BRICKS)),name="diff?")
+    abs_var = m.addVars(N_BRICKS,vtype=gp.GRB.INTEGER,name="abs")
+    m.addConstrs((abs_var[i] == gp.abs_(diff_var[i]) for i in range(N_BRICKS)),name="abs?")
+    disrupt_sum = gp.quicksum(abs_var[i] for i in range(N_BRICKS))
     m.setObjective(disrupt_sum, gp.GRB.MINIMIZE)
-
-    m.write("myModel.lp")
-    #Create Objective
-    #Solve
     m.optimize()
-    restemp = []
-    for i in range(nb_bricks):
-        l=[]
-        for j in range(nb_SR):
-            l.append(abs_var[i,j].x)
-        restemp.append(l)
-    print(restemp)
+    # restemp = []
+    # for i in range(N_BRICKS):
+    #     l=[]
+    #     for j in range(NB_SR):
+    #         l.append(abs_var[i,j].x)
+    #     restemp.append(l)
+    # print(restemp)
 
-
-# elif OBJECTIVE == "dist":
-    #TO DO: Objective 2:still broken because of center
-    # mult_mat = m.addVars(nb_bricks,nb_SR,vtype=gp.GRB.CONTINUOUS,name="M")
-    
-    
-    # dist_temp = m.addMVar(shape=(nb_bricks, nb_unfixed_center), vtype=gp.GRB.CONTINUOUS, name="D")
-    
-    # m.addConstrs((dist_temp[i,k] == gp.quicksum(center_bricks[j]*distance_matrix[i,j] 
-    #                                             for j in range(nb_bricks) if j not in center)
-    #               for i in range(nb_bricks) for k in range(nb_unfixed_center)), name="tempdist")
-    
-
-    
-    # m.addConstrs((mult_mat[i,j] == v[i,j]*distances[i,j] for i in range(nb_bricks) for j in range(len(center))), name="ee")
-    # m.addConstrs((mult_mat[i,j] == v[i,j]*dist_temp[i] for i in range(nb_bricks) for j in range(len(center),nb_SR)),
-    #              name="Part2")
-    # dist_sum = gp.quicksum(mult_mat[i,j] for i in range(nb_bricks) for j in range(nb_SR))
-    
-    # m.setObjective(dist_sum, gp.GRB.MINIMIZE)
-    # m.write("myModel.lp")
-    # #Create Objective
-    # #Solve
-    # m.optimize() 
-
+elif OBJECTIVE == "dist":
+    mult_mat = m.addVars(N_BRICKS,NB_SR,vtype=gp.GRB.CONTINUOUS)
+    m.addConstrs(mult_mat[i,j] == v[i,j]*distances[i,j] for i in range(N_BRICKS) for j in range(NB_SR))
+    dist_sum = gp.quicksum(mult_mat[i,j] for i in range(N_BRICKS) for j in range(NB_SR))
+    m.setObjective(dist_sum, gp.GRB.MINIMIZE)
+    m.optimize() 
     # dist_mat = []
-    # for i in range(nb_bricks):
+    # for i in range(N_BRICKS):
     #     row = []
-    #     for j in range(nb_SR):
+    #     for j in range(NB_SR):
     #         row.append(mult_mat[i,j].x)
     #     dist_mat.append(row)
     
@@ -155,42 +123,41 @@ if OBJECTIVE == "disrupt":
 
 # new_center = -1
 # if nb_additional_SR > 0:
-#     for i in range(nb_bricks):
+#     for i in range(N_BRICKS):
 #         if center_bricks[i].x > 0:
 #             new_center = i
 #     print(f"nouveau SR en {i}")
 print("La solution est :")
 res = []
-for j in range(nb_SR):
+for j in range(NB_SR):
     sale_j = []
-    for i in range(nb_bricks):
+    for i in range(N_BRICKS):
         if v[i,j].x == 1:
             sale_j.append(i+1)
     res.append(sale_j)
 res2 = []
-for i in range(nb_bricks):
+for i in range(N_BRICKS):
     if center_bricks[i].x == 1:
         res2.append(i+1)
-
 print(res)
+print("Centers :", res2)
 # print(f"new_center : {res2} // old : [3,13,15,21]")
 
 #print workload matrix
-workload_res = []
-for i in range(nb_bricks):
-    row = []
-    for j in range(nb_SR):
-        row.append(resulting_workload[i,j].x)
-    workload_res.append(row)
+# workload_res = []
+# for i in range(N_BRICKS):
+#     row = []
+#     for j in range(NB_SR):
+#         row.append(resulting_workload[i,j].x)
+#     workload_res.append(row)
 
-workload_res = np.array(workload_res)
-print(workload_res)
+# workload_res = np.array(workload_res)
+# print(workload_res)
 
-sums = []
-for i in range(nb_SR):
-    sums.append(weighted_sums[i].x)
-
-print(sums)
+# sums = []
+# for i in range(NB_SR):
+#     sums.append(weighted_sums[i].x)
+# print(sums)
 
 
 ################################
